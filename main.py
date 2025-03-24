ee2a8ec0a87949a93f0093b558de5d760ef66d59
bishoy <bishoyw.fathy@gmail.com>
2025-03-24 17:40:15 +0200

    add basic board UI

--- main.py
+++ main.py
@@ -1,64 +1,79 @@
 import pygame as pg
 import sys
+import random
 import gi
+from board import Board
+import math
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 from gettext import gettext as _
 
-# Configuration constants
-WIDTH, HEIGHT = 800, 600
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+import config
 
+# The main controller
 class Main:
     def __init__(self, journal=True):
         self.journal = journal
         self.running = True
-        self.screen = None
-        self.font = None
+        self.canvas = None
+        self.moves = 0
+        self.show_help = False
+        self.help_img = None  # We'll need to create a help image for 15 puzzle
+        self.board = None
+        self.solved = False
+        self.fancy_button_anim = 0  # Animation counter for the fancy button
 
     def set_canvas(self, canvas):
-        self.screen = canvas
-        pg.display.set_caption(_("Hello World App"))
+        self.canvas = canvas
+        pg.display.set_caption(_("15 Puzzle"))
 
     def quit(self):
         self.running = False
 
-    def check_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.quit()
-
     def draw(self):
-        self.screen.fill(WHITE)
-        text = self.font.render(_("Hello, World!"), True, BLACK)
-        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        self.screen.blit(text, text_rect)
+        config.WIN.fill(config.BLACK)
+        # Draw the board
+        self.board.draw()
         pg.display.update()
 
+    def reset_game(self):
+        self.board = Board(self, (config.WIDTH / 2, config.HEIGHT / 2))
+        self.moves = 0
+        self.solved = False
+
+    # The main loop
     def run(self):
-        pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT), pg.RESIZABLE)
-        self.font = pg.font.Font(None, 72)
+        for event in pg.event.get():
+            if event.type == pg.VIDEORESIZE:
+                pg.display.set_mode(event.size, pg.RESIZABLE)
+                break
+        config.init()
+        pg.font.init()
+        if self.canvas is not None:
+            self.canvas.grab_focus()
 
-        if self.screen is not None:
-            self.screen.fill(WHITE)
-            pg.display.flip()
-
+        self.reset_game()
         while self.running:
             if self.journal:
+                # Pump GTK messages.
                 while Gtk.events_pending():
                     Gtk.main_iteration()
 
-            self.check_events()
             self.draw()
-            pg.time.Clock().tick(30)
-
+        pg.display.quit()
         pg.quit()
         sys.exit(0)
 
+
+# Test if the script is directly ran
 if __name__ == "__main__":
+    pg.init()
+    # Set your desired width and height
+    info = pg.display.Info()
+    screen_width, screen_height = info.current_w, info.current_h
+    # Set the display mode with the specified width and height
+    screen = pg.display.set_mode((screen_width, screen_height), pg.FULLSCREEN)
     main = Main(journal=False)
     main.run()
